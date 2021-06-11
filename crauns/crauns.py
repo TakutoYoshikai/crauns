599a6834e89fdeb1c7bb90863ab2d2a6c5ffc19e
lucky <takuto.yoshikai@gmail.com>
2021-06-11 19:17:16 +0900

    rename

--- crauns/crauns.py
+++ crauns/crauns.py
@@ -3,6 +3,8 @@ from Crypto.PublicKey import RSA
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 import base64
+import sys
+import argparse
 
 def set_key(image_path, name, privkey_path):
     privkey = RSA.import_key(open(privkey_path, "rb").read())
@@ -21,4 +23,18 @@ def verify(image_path, name, pubkey_path):
         return False
     return True
 
+def main():
+    parser = argparse.ArgumentParser(description="crauns")
+    parser.add_argument("mode", help="set or verify")
+    parser.add_argument("-i", "--image", help="image")
+    parser.add_argument("-n", "--name", help="name")
+    parser.add_argument("-v", "--priv", help="private key")
+    parser.add_argument("-p", "--pub", help="public key")
+    args = vars(parser.parse_args())
+    if args["mode"] == "set":
+        set_key(args["image"], args["name"], args["priv"])
+    elif args["mode"] == "verify":
+        verify(args["image"], args["name"], args["pub"])
 
+if __name__ == "__main__":
+    main()
