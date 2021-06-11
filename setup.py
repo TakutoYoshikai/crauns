59f5b76333dceb1b8c6e2320d62b95734b16476f
lucky <takuto.yoshikai@gmail.com>
2021-06-11 19:19:29 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'crauns',
+    version = '1.0.0',
+    url = 'https://github.com/TakutoYoshikai/crauns.git',
+    license = 'MIT LICENSE',
+    author = 'Takuto Yoshikai',
+    author_email = 'takuto.yoshikai@gmail.com',
+    description = 'crauns',
+    install_requires = ['setuptools', "pycryptodome", "stegano"],
+    packages = find_packages(),
+    entry_points={
+        "console_scripts": [
+            "crauns = crauns.crauns:main",
+        ]
+    }
+)
